ee909bcb29acb518c1f07fae93d1009c1c7b47ac
Rudragouda Patil <patil.rudragouda2@gmail.com>
2019-04-16 10:29:33 +0530

    first commit

--- App.js
+++ App.js
@@ -25,12 +25,10 @@ export default class App extends Component<Props> {
   render() {
     return (
       // <View style={styles.container}>
-      //   <Text style={styles.welcome}>Welcome to React Native!</Text>
-      //   <Text style={styles.instructions}>To get started, edit App.js</Text>
-      //   <Text style={styles.instructions}>{instructions}</Text>
+      <Additems/>
+    // <Registration/>
       // </View>
-     // <Additems/>
-  <Registration/>
+ 
  //   <Navtab/>
      );
   }
